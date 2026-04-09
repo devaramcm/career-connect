eb160287156b67160fe517386ff42a6ce5cab03d
devaramcm <devaramcm05@gmail.com>
2026-04-09 11:16:12 +0530

    backend register fix

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -22,7 +22,7 @@ export default function Register() {
     }
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/register", {
+      const res = await fetch("https://career-connect-lk0b.onrender.com/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
